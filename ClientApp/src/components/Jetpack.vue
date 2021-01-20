19cb856af2e90e13463c775740481d2adcdf9936
MhozaifaA <botchey.mha@hotmail.com>
2021-01-20 20:26:39 +0200

    player

--- ClientApp/src/components/Jetpack.vue
+++ ClientApp/src/components/Jetpack.vue
@@ -5,11 +5,12 @@
     <button v-if="countdie==0" class="btn btn-outline-success my-1" @click="restart">Play</button>
 
     <div class="game" v-bind:class="losteffect">
-       
+
         <groundspace></groundspace>
 
-        <div id="palyer" v-bind:class="changeClass" v-bind:style="{top:player_top+'px',left:player_left+'px'}" />
-       
+        <player :player_top="player_top" :player_left="player_left" :changeClass="changeClass"></player>
+
+
         <div v-for="enemy in enemies" v-bind:key="enemy.id"
              v-bind:style="{top:enemy.top+'px',left:enemy.left+'px',width:enemy.size+'px',height:enemy.size+'px'}"
              v-bind:class="enemy.class">
@@ -17,14 +18,16 @@
         </div>
 
         <div v-for="fire in fires" v-bind:key="fire" class="fireball" :style="{top:fire.top+'px',left:fire.left+'px'}"></div>
-    </div>
 
+    </div>
+   
 </template>
 <!--<div v-for="enemy in enemies" v-bind:key="enemy.id"
       v-bind:style="{top:enemy.top+'px',left:enemy.left+'px',width:enemy.size+'px',height:enemy.size+'px'}"
       v-bind:class="enemy.class"> </div>-->
 <script>
     import groundspace from "./groundspace"
+    import player from "./player"
 
 
     function delay(ms) {
@@ -72,6 +75,7 @@
 
         components: {
             groundspace,
+            player
         },
 
         data() {
@@ -508,146 +512,6 @@
     * {
         user-select: none;
         user-drag: none;
-    }
-
-
-    #palyer {
-        position: absolute;
-        height: 150px;
-        width: 120px;
-        left: 160px;
-        top: 180px;
-        /*transition: top 0.5s linear;*/
-    }
-
-    .down {
-        background: url(../assets/man/down.png) no-repeat;
-    }
-
-    .flay {
-        animation: flay 0.6s linear infinite;
-    }
-
-    .run {
-        animation: run 0.6s linear infinite;
-    }
-
-    .stand {
-        background: url(../assets/man/stand.png) no-repeat;
-    }
-
-    .shootflay {
-        animation: shootflay 0.6s linear infinite;
-    }
-
-    .die {
-        animation: die 1s linear infinite;
-    }
-    @keyframes die {
-        0% {
-            background: url(../assets/man/die/7.png) no-repeat;
-        }
-
-        33.3% {
-            background: url(../assets/man/die/8.png) no-repeat;
-        }
-
-        66.6% {
-            background: url(../assets/man/die/9.png) no-repeat;
-        }
-
-        100% {
-            background: url(../assets/man/die/10.png) no-repeat;
-        }
-    }
-
-
-    @keyframes shootflay {
-        0% {
-            background: url(../assets/man/shootflay/1.png) no-repeat;
-        }
-
-        25% {
-            background: url(../assets/man/shootflay/2.png) no-repeat;
-        }
-
-        50% {
-            background: url(../assets/man/shootflay/3.png) no-repeat;
-        }
-
-        75% {
-            background: url(../assets/man/shootflay/4.png) no-repeat;
-        }
-
-        100% {
-            background: url(../assets/man/shootflay/1.png) no-repeat;
-        }
-    }
-
-    @keyframes run {
-        0% {
-            background: url(../assets/man/run/1.png) no-repeat;
-        }
-
-        20% {
-            background: url(../assets/man/run/2.png) no-repeat;
-        }
-
-        40% {
-            background: url(../assets/man/run/3.png) no-repeat;
-        }
-
-        60% {
-            background: url(../assets/man/run/4.png) no-repeat;
-        }
-
-        80% {
-            background: url(../assets/man/run/5.png) no-repeat;
-        }
-        /* 50% {
-            background: url(../assets/man/run/6.png);
-        }
-        60% {
-            background: url(../assets/man/run/7.png);
-        }
-        70% {
-            background: url(../assets/man/run/8.png);
-        }
-        80% {
-            background: url(../assets/man/run/9.png);
-        }
-        90% {
-            background: url(../assets/man/run/10.png);
-        }*/
-        100% {
-            background: url(../assets/man/run/1.png) no-repeat;
-        }
-    }
-
-    @keyframes flay {
-        0% {
-            background: url(../assets/man/flay/1.png) no-repeat;
-        }
-
-        20% {
-            background: url(../assets/man/flay/2.png) no-repeat;
-        }
-
-        40% {
-            background: url(../assets/man/flay/3.png) no-repeat;
-        }
-
-        60% {
-            background: url(../assets/man/flay/4.png) no-repeat;
-        }
-
-        80% {
-            background: url(../assets/man/flay/5.png) no-repeat;
-        }
-
-        100% {
-            background: url(../assets/man/flay/1.png) no-repeat;
-        }
     }
 
     .game {
---
+++ ClientApp/src/components/player.vue
@@ -0,0 +1,159 @@
+﻿<template>
+    <div id="palyer" v-bind:class="changeClass" v-bind:style="{top:player_top+'px',left:player_left+'px'}" />
+</template>
+
+
+
+
+<script>
+    export default {
+        name: "player",
+        props: {
+            player_top: Number,
+            player_left: Number,
+            changeClass: String
+        },
+    }
+</script>
+
+<style scoped>
+    #palyer {
+        position: absolute;
+        height: 150px;
+        width: 120px;
+        left: 160px;
+        top: 180px;
+        /*transition: top 0.5s linear;*/
+    }
+
+    .down {
+        background: url(../assets/man/down.png) no-repeat;
+    }
+
+    .flay {
+        animation: flay 0.6s linear infinite;
+    }
+
+    .run {
+        animation: run 0.6s linear infinite;
+    }
+
+    .stand {
+        background: url(../assets/man/stand.png) no-repeat;
+    }
+
+    .shootflay {
+        animation: shootflay 0.6s linear infinite;
+    }
+
+    .die {
+        animation: die 1s linear infinite;
+    }
+
+    @keyframes die {
+        0% {
+            background: url(../assets/man/die/7.png) no-repeat;
+        }
+
+        33.3% {
+            background: url(../assets/man/die/8.png) no-repeat;
+        }
+
+        66.6% {
+            background: url(../assets/man/die/9.png) no-repeat;
+        }
+
+        100% {
+            background: url(../assets/man/die/10.png) no-repeat;
+        }
+    }
+
+
+    @keyframes shootflay {
+        0% {
+            background: url(../assets/man/shootflay/1.png) no-repeat;
+        }
+
+        25% {
+            background: url(../assets/man/shootflay/2.png) no-repeat;
+        }
+
+        50% {
+            background: url(../assets/man/shootflay/3.png) no-repeat;
+        }
+
+        75% {
+            background: url(../assets/man/shootflay/4.png) no-repeat;
+        }
+
+        100% {
+            background: url(../assets/man/shootflay/1.png) no-repeat;
+        }
+    }
+
+    @keyframes run {
+        0% {
+            background: url(../assets/man/run/1.png) no-repeat;
+        }
+
+        20% {
+            background: url(../assets/man/run/2.png) no-repeat;
+        }
+
+        40% {
+            background: url(../assets/man/run/3.png) no-repeat;
+        }
+
+        60% {
+            background: url(../assets/man/run/4.png) no-repeat;
+        }
+
+        80% {
+            background: url(../assets/man/run/5.png) no-repeat;
+        }
+        /* 50% {
+            background: url(../assets/man/run/6.png);
+        }
+        60% {
+            background: url(../assets/man/run/7.png);
+        }
+        70% {
+            background: url(../assets/man/run/8.png);
+        }
+        80% {
+            background: url(../assets/man/run/9.png);
+        }
+        90% {
+            background: url(../assets/man/run/10.png);
+        }*/
+        100% {
+            background: url(../assets/man/run/1.png) no-repeat;
+        }
+    }
+
+    @keyframes flay {
+        0% {
+            background: url(../assets/man/flay/1.png) no-repeat;
+        }
+
+        20% {
+            background: url(../assets/man/flay/2.png) no-repeat;
+        }
+
+        40% {
+            background: url(../assets/man/flay/3.png) no-repeat;
+        }
+
+        60% {
+            background: url(../assets/man/flay/4.png) no-repeat;
+        }
+
+        80% {
+            background: url(../assets/man/flay/5.png) no-repeat;
+        }
+
+        100% {
+            background: url(../assets/man/flay/1.png) no-repeat;
+        }
+    }
+</style>
