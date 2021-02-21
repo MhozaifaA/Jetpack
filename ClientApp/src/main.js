52d946f3fc188d34d0f72b44f8399e603b00d724
MhozaifaA <botchey.mha@hotmail.com>
2021-02-21 22:57:49 +0200

    vuetify

--- ClientApp/src/main.js
+++ ClientApp/src/main.js
@@ -2,5 +2,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app')
