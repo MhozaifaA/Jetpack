c9c96d9cad1b757ad346a54ec165bd04d8103bc2
MhozaifaA <botchey.mha@hotmail.com>
2021-02-21 23:00:22 +0200

    Revert "vuetify"
    This reverts commit 52d946f3fc188d34d0f72b44f8399e603b00d724.

--- ClientApp/src/main.js
+++ ClientApp/src/main.js
@@ -2,6 +2,5 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
 
-createApp(App).use(router).use(vuetify).mount('#app')
+createApp(App).use(router).mount('#app')
